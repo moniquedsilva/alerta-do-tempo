d0dd44d910fa2477654495489935b0a6a7125aec
Monique Silva <moniquesntsilva@gmail.com>
2022-05-23 23:10:02 -0300

    adiciona controller da home

---
+++ main/controllers/HomeController.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.views import View
+
+class HomeController(View):
+    def get(self, request, *args, **kwargs):
+        return render(request,'home.html')
