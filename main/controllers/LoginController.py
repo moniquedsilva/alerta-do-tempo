21e1171006074339bb90fbeac896270a6fd1fe04
Milena <elenprojects7@gmail.com>
2022-05-29 00:47:35 -0300

    add controller

--- main/controllers/LoginController.py
+++ main/controllers/LoginController.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from django.views import View
 
+from django.http import HttpResponse, JsonResponse
+from main.services.ClienteService import ClienteService
+
+from django.contrib.auth.models import Permission
+from django.contrib.contenttypes.models import ContentType
 
 class LoginController(View):
     def get(self, request, *args, **kwargs):
         return render(request, 'login.html')
+
+
+    def login(request):
+        nome = request.POST['nome'];
+        senha = request.POST['senha'];
