18512be1bcc53c7ed87297045296d7e0e4f521f8
Monique Silva <moniquesntsilva@gmail.com>
2022-06-05 00:46:14 -0300

    algumas alteracoes na funcao login

--- main/controllers/LoginController.py
+++ main/controllers/LoginController.py
@@ -1,34 +1,31 @@
-from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import Permission, User
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.sessions.models import Session
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import redirect, render
 from django.views import View
 
-from django.http import HttpResponse, JsonResponse
 from main.services.ClienteService import ClienteService
 
-from django.contrib.auth.models import Permission
-from django.contrib.auth import authenticate, login, logout
-from django.contrib import messages
-from django.contrib.contenttypes.models import ContentType
 
 class LoginController(View):
     def get(self, request, *args, **kwargs):
         return render(request, 'login.html')
 
-
-    def login_user(self, request):
+    def login_user(request):
         if request.method == 'POST':
-            nome = request.POST['nome'];
-            celular = request.POST['celular'];
-            user = authenticate(request, username=nome, celular=celular)
-        if user is not None:
-            login(request, user)
-            return redirect('')
-            # Redirect to a success page.
-        else:
-            messages.error(request, 'Error')
-            return redirect('login')
-
+            nome = request.POST['nome']
+            celular = request.POST['celular']
+            user = authenticate(request, username=nome, password=celular)
+            if user is not None:
+                login(request, user)
+                return HttpResponse('Funcionou')
+            else:
+                return HttpResponse('Não funcionou')
 
     def logout_user(request):
         logout(request)
-        messages.info(request, "You have successfully logged out.") 
+        messages.info(request, "You have successfully logged out.")
         return redirect('')
