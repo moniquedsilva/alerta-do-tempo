350aa564ef00e7f77ede02ceb53f757655098ef4
Monique Silva <moniquesntsilva@gmail.com>
2022-06-07 11:55:37 -0300

    documenta controllers e sonar fixes

--- main/controllers/HomeController.py
+++ main/controllers/HomeController.py
@@ -3,5 +3,11 @@ from django.views import View
 
 
 class HomeController(View):
+
     def get(self, request, *args, **kwargs):
+        '''
+        Renderiza a home.
+        :param request: requisão HTTP GET.
+        :return: render(request, 'home.html').
+        '''
         return render(request, 'home.html')
