f4db1cf5fec2b48ed2a48e333b03648be3c87c3d
Monique Silva <moniquesntsilva@gmail.com>
2022-06-16 14:10:15 -0300

    adiciona condicional para icons e ajustes

--- main/resources/js/dashboard.js
+++ main/resources/js/dashboard.js
@@ -39,6 +39,25 @@ window.addEventListener("load", function () {
         return `${dd}/${mm}/${yyyy}`;
     }
 
+    function climaIcon(tipo) {
+        if (
+            tipo === "ci" ||
+            tipo === "c" ||
+            tipo === "ch" ||
+            tipo === "pp" ||
+            tipo === "pc"
+        )
+            return "chuvoso";
+        else if (tipo === "np" || tipo === "pn" || tipo === "n" || tipo === "e")
+            return "nublado";
+        else if (tipo === "t") return "tempestade";
+        else if (tipo === "ps" || tipo === "cl") return "ensolarado";
+        else if (tipo === "g" || tipo === "ne") return "nevada";
+        else {
+            return "parcialmente-nublado";
+        }
+    }
+
     function respostaDados(dadosJSON) {
         let main = document.getElementById("alerts");
         main.innerHTML = "";
@@ -46,14 +65,18 @@ window.addEventListener("load", function () {
         const { chuvas_iuv } = dados;
         chuvas_iuv.lista_previsao.forEach((item) => {
             const dia = formatarData(item.dia);
+            const icon = climaIcon(item.tempo);
             let alerta = `
                 <div class="dashboard__alerts-conteudo">
-                    <h2>${dia} </h2>
-                    <div class="dashboard__alerts-temperatura">
-                        <h3>Máxima: ${item.maxima}</h3>
-                        <h3>Mínima: ${item.minima}</h3>
+                    <div class="dashboard__alerts-texto">
+                        <h2>${dia}</h2>
+                        <div class="dashboard__alerts-temperatura">
+                            <h3>Máxima: ${item.maxima}</h3>
+                            <h3>Mínima: ${item.minima}</h3>
+                        </div>
+                        <p class="dashboard__alerts-tipo">${item.tempo_descricao}</p>
                     </div>
-                    <p class="dashboard__alerts-tipo">${item.tempo_descricao}</p>
+                    <img src="/main/resources/assets/images/${icon}.png" alt="${item.tempo}-tempo" class="dashboard__alerts-img" />
                 </div>
             `;
 
