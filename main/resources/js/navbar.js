ca54b0caf6ba49a0cd8602dd30cedafd1fc87683
Monique Silva <moniquesntsilva@gmail.com>
2022-06-28 21:09:07 -0300

    corrige erro na navbar

--- main/resources/js/navbar.js
+++ main/resources/js/navbar.js
@@ -1,12 +1,8 @@
 window.addEventListener("load", function () {
     // Menu mobile
     const nav = document.querySelector(".nav-toggle");
-    const links = document.getElementById("links");
+    const links = document.querySelector(".links");
     nav.addEventListener("click", function () {
-        if (links.style.height === "auto") {
-            links.style.height = "12rem";
-        } else {
-            links.style.height = "auto";
-        }
+        links.classList.toggle("show-links");
     });
 });
